cc8e02540fe17a89cafed40b9ae47955ce5ef618
Erdal Günay <gunayerdal@hotmail.com>
2016-12-29 15:28:11 +0300

    add buton added

--- SpringMVC/src/main/webapp/assets/js/main.js
+++ SpringMVC/src/main/webapp/assets/js/main.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
         language: {
             search: "_INPUT_",
             searchPlaceholder: "Search"
+        },
+        'fnDrawCallback': function (oSettings) {
+            $('.dataTables_filter').each(function () {
+                $(this).append('<a href="#" class="btn btn-success" data-toggle="addModal" style="margin-left: 15px"><span class="glyphicon glyphicon-plus"></span> ' +
+                'Add</a>');
+            });
         }
     } );
 } );
